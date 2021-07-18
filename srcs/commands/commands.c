38c5091f0dc8e2bcf46e988661a2ef617123b2d3
mgroot <mgroot@students.school-21.ru>
2021-07-18 13:15:48 +0300

    Command_processing prepare: splited to 3 blocks (pipe, redir and exec)

--- srcs/commands/commands.c
+++ srcs/commands/commands.c
@@ -6,50 +6,20 @@ static int	exec_process(t_commlist *commands, t_envp *envp);
 
 int	command_processing(t_commlist *commands, t_envp *envp)
 {
-	int		pipe_fds[2];
-	int		prev_fd;
 	int		status;
-	int		pid;
 
 	//commlist_print(commands);
-	prev_fd = -1;
-	while (commands)
-	{
-		if (commands->op_next == OP_PIPE && pipe(pipe_fds) == -1)
-		{
-			perror("Can't create pipe ");
-			return (-1);
-		}
-		pid = fork();
-		if (pid < 0)
-		{
-			perror("Can't fork ");
-			return (-2);
-		}
-		else if (pid == 0)
-		{
-			if (prev_fd >= 0)
-				dup2(prev_fd, STDIN_FILENO);
-			if (commands->op_next == OP_PIPE)
-				dup2(pipe_fds[1], STDOUT_FILENO);
-			handle_command(commands->argv, envp);
-		}
-		if (prev_fd >= 0)
-			close(prev_fd);
-		if (commands->op_next == OP_PIPE)
-		{
-			close(pipe_fds[1]);
-			prev_fd = pipe_fds[0];
-		}
-		else
-			prev_fd = -1; 
-		commands = commands->next;
-	}
+	if (pipe_parse(commands) != 0)
+		return (-1);
+	if (redir_parse(commands) != 0)
+		return (-2);
+	if (exec_process(commands, envp) != 0)
+		return (-3);
 	wait(&status);
 	if (status == -1)
 	{
-		perror("Wait error ");
-		return (-3);
+		perror("Wait error");
+		return (-4);
 	}
 	return (0);
 }
