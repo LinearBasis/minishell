11e898cf4b5c867a682019dc29eb6b3ce2cfa58e
mgroot.lablet <pp189.mgroot@gmail.com>
2021-07-21 08:44:10 +0300

    Parser norminetted

--- srcs/parser/envp_replace.c
+++ srcs/parser/envp_replace.c
@@ -1,13 +1,11 @@
 #include "parser.h"
 
-static char		*envp__get_value(char **envp[2], char *key, size_t *key_len);
 static void		replace_keys(char *str, char *dest, char **envp[2],
 					char *exit_code);
 static char		*replace_keys__key(char *str, char **envp[2],
 					char *exit_code, size_t *key_len);
-static size_t	get_new_len(char *str, char **envp[2], char *exit_code);
-static size_t	get_new_len__key(char *str, char **envp[2],
-					char *exit_code, size_t *key_len);
+static void		replace_keys__copy_n_move(char **dest, char **src,
+					int to_zero_flag);
 
 int	parser__envp_replace(char **str, t_envp *envp, int last_exit_code)
 {
@@ -16,7 +14,7 @@ int	parser__envp_replace(char **str, t_envp *envp, int last_exit_code)
 	char	*exit_code;
 
 	exit_code = ft_itoa(last_exit_code);
-	size = get_new_len(*str, envp->envp_key_value, exit_code);
+	size = replace_keys__get_new_len(*str, envp->envp_key_value, exit_code);
 	out = malloc(sizeof(char) * (size + 1));
 	if (!out)
 	{
@@ -31,42 +29,23 @@ int	parser__envp_replace(char **str, t_envp *envp, int last_exit_code)
 	return (0);
 }
 
-static size_t	get_new_len(char *str, char **envp[2], char *exit_code)
+char	*replace_keys__get_value(char **envp[2], char *key, size_t *key_len)
 {
-	size_t	size;
-	size_t	key_len;
-	int		squotes_flag;
+	size_t	index;
 
-	squotes_flag = 0;
-	size = 0;
-	while (*str)
+	*key_len = 0;
+	while (key[*key_len] && !ft_isspace(key[*key_len])
+		&& parser__is_oper(key) == OP_NONE && key[*key_len] != '\"'
+		&& key[*key_len] != '$' && key[*key_len] != '=')
+		++(*key_len);
+	index = 0;
+	while (envp[KEY][index])
 	{
-		key_len = 0;
-		if (*str == '\'')
-			squotes_flag = !squotes_flag;
-		else if (*str == '$' && !squotes_flag && *(str + 1)
-			&& !ft_isspace(*(str + 1)) && *(str + 1) != '=')
-			size += get_new_len__key(str, envp, exit_code, &key_len);
-		else
-			++size;
-		str += key_len + 1;
+		if (ft_strncmp(envp[KEY][index], key, *key_len) == 0)
+			return (envp[VALUE][index]);
+		index++;
 	}
-	return (size);
-}
-
-static size_t	get_new_len__key(char *str, char **envp[2],
-					char *exit_code, size_t *key_len)
-{
-	size_t	out_size;
-
-	if (*(str + 1) == '?')
-	{
-		out_size += ft_strlen(exit_code);
-		key_len = 1;
-	}
-	else if (*(str + 1))
-		out_size += ft_strlen(envp__get_value(envp, str + 1, key_len));
-	return (out_size);
+	return (NULL);
 }
 
 static void	replace_keys(char *str, char *dest, char **envp[2],
@@ -85,20 +64,12 @@ static void	replace_keys(char *str, char *dest, char **envp[2],
 			&& !ft_isspace(*(str + 1)) && *(str + 1) != '=')
 		{
 			value = replace_keys__key(str, envp, exit_code, &key_len);
-			while (value && *value)
-			{
-				*dest = *value;
-				++dest;
-				++value;
-			}
+			if (value)
+				replace_keys__copy_n_move(&dest, &value, 1);
 			str += key_len + 1;
 		}
 		else
-		{
-			*dest = *str;
-			++dest;
-			++str;
-		}
+			replace_keys__copy_n_move(&dest, &str, 0);
 	}
 }
 
@@ -115,25 +86,26 @@ static char	*replace_keys__key(char *str, char **envp[2],
 		*key_len = 1;
 	}
 	else if (*(str + 1))
-		value = envp__get_value(envp, str + 1, key_len);
+		value = replace_keys__get_value(envp, str + 1, key_len);
 	return (value);
 }
 
-static char	*envp__get_value(char **envp[2], char *key, size_t *key_len)
+static void	replace_keys__copy_n_move(char **dest, char **src,
+				int to_zero_flag)
 {
-	size_t	index;
-
-	*key_len = 0;
-	while (key[*key_len] && !ft_isspace(key[*key_len])
-		&& parser__is_oper(key) == OP_NONE && key[*key_len] != '\"'
-		&& key[*key_len] != '$' && key[*key_len] != '=')
-		++(*key_len);
-	index = 0;
-	while (envp[KEY][index])
+	if (to_zero_flag)
 	{
-		if (ft_strncmp(envp[KEY][index], key, *key_len) == 0)
-			return (envp[VALUE][index]);
-		index++;
+		while (**src)
+		{
+			**dest = **src;
+			*dest += 1;
+			*src += 1;
+		}
 	}
-	return (NULL);
+	else
+	{
+		**dest = **src;
+		*dest += 1;
+		*src += 1;
+	}
 }
