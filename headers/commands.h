9b7046324dd51d6f36aa1a7eb4d0691876600ce7
mgroot <mgroot@students.school-21.ru>
2021-07-22 10:57:04 +0300

    Commands.h update

--- headers/commands.h
+++ headers/commands.h
@@ -13,12 +13,6 @@
 # include "utils.h"
 # include "error.h"
 
-# define EXECERR__SUCCESS	0
-# define EXECERR__PATHDIRS	1
-# define EXECERR__HOMEDIR	2
-# define EXECERR__EXECVE	126
-# define EXECERR__NOT_FOUNT 127
-
 int		command_processing(t_commlist **commands, t_envp *envp);
 int		commands__pipe_parser(t_commlist *commands);
 
